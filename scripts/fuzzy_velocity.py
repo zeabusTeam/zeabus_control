0e8a607fb967fd8c112b81317acbecc767a41cc8
SupasanKomonlit <supasan_224@hotmail.com>
2019-12-18 12:34:38 +0700

    Create way to choose velocity

---
+++ scripts/fuzzy_velocity.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python2
+# FILE			: fuzzy_velocity.py
+# AUTHOR		: K.Supasan
+# CREATE ON		: 2019, December 14 (UTC+0)
+# MAINTAINER	: K.Supasan
+
+# README
+#   This file will be interface to connect with other node
+#   This node have responsible to navigate wii send 
+
+# REFERENCE
+
+class TABLEZ( object ):
+    ERROR_POSITION = ( -2.5 , -1.0 , 0.0 , 0.1 , 1 , 2.5 )
+    CURRENT_VELOCITY = ( -0.5 , -0.1 , 0 , +0.1 , +0.5 )
+    ACCELERATION = ( -0.1 , -0.05 , 0 , +0.05 , +0.1 )
+
+class TABLEROLL( object ):
+    ERROR_POSITION = ( -1 , -0.5 , 0 , 0.5 , 1 )
+    CURRENT_VELOCITY = ( -0.5 , -0.25 , 0 , 0.25 , 0.5 )
+    ACCELERATION = ( -0.25 , -0.1 , 0 , 0.1 , 0.25 )
+
+class TABLEPITCH( object ):
+    ERROR_POSITION = ( -1 , -0.5 , 0 , 0.5 , 1 )
+    CURRENT_VELOCITY = ( -0.5 , -0.25 , 0 , 0.25 , 0.5 )
+    ACCELERATION = ( -0.25 , -0.1 , 0 , 0.1 , 0.25 )
+
+class TABLEYAW( object ):
+    ERROR_POSITION = ( -1 , -0.5 , 0 , 0.5 , 1 )
+    CURRENT_VELOCITY = ( -0.5 , -0.25 , 0 , 0.25 , 0.5 )
+    ACCELERATION = ( -0.25 , -0.1 , 0 , 0.1 , 0.25 )
+     
+class FuzzyVelocity:
+    
+    def __init__( self , mode ):
+        if mode == 'z':
+            self.set_error_position( TABLEZ.ERROR_POSITION )
+            self.set_current_velocity( TABLEZ.CURRENT_VELOCITY )
+            self.set_acceleration( TABLEZ.ACCELERATION )
+        elif mode == 'roll' :
+            self.set_error_position( TABLEROLL.ERROR_POSITION )
+            self.set_current_velocity( TABLEROLL.CURRENT_VELOCITY )
+            self.set_acceleration( TABLEROLL.ACCELERATION )
+        elif mode == 'pitch' :
+            self.set_error_position( TABLEPITCH.ERROR_POSITION )
+            self.set_current_velocity( TABLEPITCH.CURRENT_VELOCITY )
+            self.set_acceleration( TABLEPITCH.ACCELERATION )
+        elif mode == "yaw" :
+            self.set_error_position( TABLEYAW.ERROR_POSITION )
+            self.set_current_velocity( TABLEYAW.CURRENT_VELOCITY )
+            self.set_acceleration( TABLEYAW.ACCELERATION )
+        else:
+            None
+
+        self.reset_summation()
+
+    def reset_summation( self ):
+        self.target_velocity = 0
+
+    def set_error_position( self , table ):
+        self.table_error = table
+        
+    def set_current_velocity( self , table ):
+        self.table_velocity = table
+
+    def set_acceleration( self , table ):
+        self.table_acceleration = table
+
+    def calculator( self , error , current_velocity ):
+        fuzzy_error = 0
+        for run in range( 0 , 4 ):
+            if run == 0 :
+                if error <= self.table_error[ run ]:
+                    fuzzy_error = run
+                    break
+                if 
+            elif run == 3 :
+                if error >= self.table_error[ run ]:
+                    fuzzy_error = run
+                    break
+            else:
+                if error > self.table_error[ run - 1 ] and error <= self.table_error[ run ] :
+                    fuzzy_error = run
+                    break
+        fuzzy_velocity = 0
+        for run in range( 0 , 4 ):
+            if run == 0 :
+                if error <= self.table_velocity[ run ]:
+                    fuzzy_velocity = run
+                    break
+                if 
+            elif run == 3 :
+                if error >= self.table_velocity[ run ]:
+                    fuzzy_velocity = run
+                    break
+            else:
+                if error > self.table_velocity[ run - 1 ] and error <= self.table_velocity[ run ] :
+                    fuzzy_velocity = run
+                    break
---
+++ scripts/step_velocity.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python2
+# FILE			: step_velocity.py
+# AUTHOR		: K.Supasan
+# CREATE ON		: 2019, December 14 (UTC+0)
+# MAINTAINER	: K.Supasan
+
+# README
+#   This file will be interface to connect with other node
+#   This node have responsible to navigate wii send 
+
+# REFERENCE
+
+class TABLEX( object ):
+    TABLE_ERROR = (  -2 , -1 , -0.1 , 0.1 , 1 , 2  )
+    TABLE_TARGET_VELOCITY = ( -1 , -0.5 , -0.2 , 0 , 0.2 , 0.5 , 1 )
+
+class TABLEY( object ):
+    TABLE_ERROR = (  -2 , -1 , -0.1 , 0.1 , 1 , 2  )
+    TABLE_TARGET_VELOCITY = ( -1 , -0.5 , -0.2 , 0 , 0.2 , 0.5 , 1 )
+
+class TABLEZ( object ):
+    TABLE_ERROR = (  -1 , -0.5 , -0.1 , 0.1 , 0.5 , 1  )
+    TABLE_TARGET_VELOCITY = ( -0.5 , -0.25 , -0.1 , 0 , 0.1 , 0.25 , 0.5 )
+
+class TABLEROLL( object ):
+    TABLE_ERROR = ( -1 , -0.5 , -0.05 , 0.05 , 0.5 , 1 )
+    TABLE_TARGET_VELOCITY = ( -0.6 , -0.3 , -0.1 , 0 , 0.1 , 0.3 , 0.6 )
+
+class TABLEPITCH( object ):
+    TABLE_ERROR = ( -1 , -0.5 , -0.05 , 0.05 , 0.5 , 1 )
+    TABLE_TARGET_VELOCITY = ( -0.6 , -0.3 , -0.1 , 0 , 0.1 , 0.3 , 0.6 )
+
+class TABLEYAW( object ):
+    TABLE_ERROR = ( -1 , -0.5 , -0.05 , 0.05 , 0.5 , 1 )
+    TABLE_TARGET_VELOCITY = ( -0.6 , -0.3 , -0.1 , 0 , 0.1 , 0.3 , 0.6 )
+
+class StepVelocity:
+    
+    def __init__( self , table , max_step = 0.05 ):
+        self.set_table_error( table.TABLE_ERROR )
+        self.set_table_velocity( table.TABLE_TARGET_VELOCITY )
+        self.reset_state()
+
+    def reset_state( self ):
+        self.save_velocity = 0
+
+    def calculate( self , error ):
+        answer = 0
+        limit_order = len( self.table_error )
+        for run in range( 0 , limit_order ):
+            if run == 0:
+                if error < self.table_error[ run ]:
+                    answer = self.table_velocity[ run ]
+            elif run == limit_order - 1 :
+                if error > self.table_error[ run ]:
+                    answer = self.table_velocity[ run + 1 ]
+            else:
+                if error >= self.table_error[ run - 1 ] and error < self.table_error[ run ]:
+                    answer = self.table_velocity[ run ]
+        return answer
+
+    def set_table_error( self , table ):
+        self.table_error = table
+
+    def set_table_velocity( self , table ):
+        self.table_velocity = table
+ 
+
